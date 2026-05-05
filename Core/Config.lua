9c5dc35909f680eac5d6b36625f524e2485fe611
Redbu11 <34152018+Redbu11dev@users.noreply.github.com>
2026-05-06 01:23:39 +0300

    Delete Core directory

--- Core/Config.lua
+++
@@ -1,7 +0,0 @@
---------------------------------------------------------------- Slash command --
--- _G.SLASH_UNITPLATES1 = "/unitplates"
--- _G.SLASH_UNITPLATES2 = "/up"
-
--- function SlashCmdList.UNITPLATES()
-	-- print("UnitPlates config is not available yet")
--- end
--- Core/Create.lua
+++
@@ -1,2 +0,0 @@
-local addon = LibStub("AceAddon-3.0"):GetAddon("UnitPlates")
-local kui = LibStub("Kui-1.0")
